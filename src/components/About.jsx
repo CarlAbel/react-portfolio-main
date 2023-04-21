9fabba5e316b10d3a533253dd56256835021136a
Carl Abel <caweb.cph@gmail.com>
2023-04-21 21:54:44 +0200

    netli u

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,8 @@ const About = () => {
       name="about"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen-lg p-4 pb-12 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-2 mt-40 md:mt-0">
+      <div className="max-w-screen-lg p-4 mb-20 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-2 mt-48 md:mt-0">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
