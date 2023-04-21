f6e286c609667ae062a07b434ab42c74721818f0
Carl Abel <caweb.cph@gmail.com>
2023-04-21 22:00:59 +0200

    sttyle rounded

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -106,7 +106,7 @@ const Experience = () => {
               onClick={() => window.open(url)}
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
-              <img src={src} alt="" className="w-20 mx-auto" />
+              <img src={src} alt="" className="w-20 mx-auto rounded-md" />
               <p className="mt-4 font-medium ">{title}</p>
               <p className="mt-4">{ratings}</p> 
             </div>
