3727d36c630279163cb06f4699e2177d4e5cce7f
Carl Abel <caweb.cph@gmail.com>
2023-04-21 21:46:34 +0200

    font updt

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -107,7 +107,7 @@ const Experience = () => {
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
               <img src={src} alt="" className="w-20 mx-auto" />
-              <p className="mt-4">{title}</p>
+              <p className="mt-4 font-medium ">{title}</p>
               <p className="mt-4">{ratings}</p> 
             </div>
           ))}
